a9b08829d3cf843ef313ef546f0d64db980d17b0
caroliniesubramaniam <70870857+caroliniesubramaniam@users.noreply.github.com>
2020-10-27 15:29:19 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+// variablen:
+//darkmode
+var darkmodeKnop = document.querySelector('#darkmode');
+var bodyElement = document.querySelector('body');
+
+//globale declaraties
+// darkmode functie
+function nightmodeKnop(event) {
+    bodyElement.classList.toggle('darkmode');
+    // darkmodeKnop.textContent = "Change to lightmode";
+}
+
+//eventlisteners 
+//darkmode
+darkmodeKnop.addEventListener('click', nightmodeKnop );
